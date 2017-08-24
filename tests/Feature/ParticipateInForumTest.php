6454d91d6bb6163defb2e9169155c6070e122afa
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-08-24 16:07:18 +0300

    Test validation errors

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Reply;
 use App\Thread;
 use App\User;
 use Tests\TestCase;
@@ -28,11 +29,25 @@ class ParticipateInForumTest extends TestCase
 
         $thread = create(Thread::class);
 
-        $reply = make(Thread::class);
+        $reply = make(Reply::class);
 
         $this->post($thread->path() . '/replies', $reply->toArray());
 
         $this->get($thread->path())
             ->assertSee($reply->body);
+    }
+
+    /** @test */
+    public function a_replay_requires_a_body()
+    {
+        $this->withExceptionHandling()
+            ->signIn();
+
+        $thread = create(Thread::class);
+
+        $reply = make(Reply::class, ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
     }
 }
