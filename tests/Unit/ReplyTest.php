900be64d32ef527385f59c8d6fb07cb521235b50
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-09-29 15:27:58 +0300

    Wrap mentioned usernames within anchor tags

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Reply;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -14,8 +15,40 @@ class ReplyTest extends TestCase
     /** @test */
     public function it_has_an_owner()
     {
-        $reply = factory(Reply::class)->create();
+        $reply = create(Reply::class);
 
         $this->assertInstanceOf('App\User', $reply->owner);
+    }
+
+    /** @test */
+    public function it_knows_it_it_was_just_published()
+    {
+        $reply = create(Reply::class);
+
+        $this->assertTrue($reply->wasJustPublished());
+
+        $reply->created_at = Carbon::now()->subMonth();
+
+        $this->assertFalse($reply->wasJustPublished());
+    }
+
+    /** @test */
+    public function it_can_detect_all_mentioned_users_in_the_body()
+    {
+        $reply = new Reply([
+            'body' => 'Hello @JaneDoe.'
+        ]);
+
+        $this->assertEquals(['JaneDoe', 'JohnDoe'], $reply->mentionedUsers());
+    }
+
+    /** @test */
+    public function it_wraps_metioned_usernames_within_the_body_in_anchor_tags()
+    {
+        $reply = new Reply([
+            'body' => 'Hello @JaneDoe.'
+        ]);
+
+        $this->assertEquals('Hello <a href="/profiles/JaneDoe">@JaneDoe</a>.', $reply->mentionedUsers());
     }
 }
