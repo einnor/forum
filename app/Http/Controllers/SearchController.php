a16f7a7d6b02ad6f82dd268283635777a8fc555c
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-11-10 10:32:16 +0300

    Set the scout driver to null during testing so to optimise running of tests and avoid syncing of created model records

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Thread;
+use App\Trending;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function show()
+    public function show(Trending $trending)
     {
         $search = request('q');
+        $threads =  Thread::search($search)->paginate(25);
 
-        return Thread::search($search)->paginate(25);
+        if(request()->expectsJson()) {
+            return $threads;
+        }
+
+        return view('threads.index', [
+            'threads'   =>  $threads,
+            'trending'  =>  $trending->get()
+        ]);
     }
 }
