d4a95469faddea784ee35c07b44a03824b270f0a
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-11-10 10:18:26 +0300

    Added a search test

---
+++ tests/Feature/SearchTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Thread;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SearchTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function a_user_can_search_threads()
+    {
+        $search = 'foobar';
+        create(Thread::class, [], 2);
+        create(Thread::class, ['body' => "A thread with the {$search} term."], 2);
+
+        $results = $this->getJson("/threads/search?q={$search}")->json();
+
+        $this->assertCount(2, $results);
+    }
+}
