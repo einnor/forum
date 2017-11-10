d8dbb495550a8752c2e4d15a9d91e4b40567fb7b
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-11-10 10:35:11 +0300

    Enable algolia and allow test to hit algolia api only if the test running is the search test

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -12,6 +12,7 @@ class SearchTest extends TestCase
     /** @test */
     public function a_user_can_search_threads()
     {
+        config(['scout.driver' => 'algolia']);
         $search = 'foobar';
         create(Thread::class, [], 2);
         create(Thread::class, ['body' => "A thread with the {$search} term."], 2);
